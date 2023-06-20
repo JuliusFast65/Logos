fa678f80054ce5fc0028701d90884519caaa0ac0
Julio <Julio@DESKTOP-DA92EIV>
2023-06-20 11:32:26 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnidadEducativa;
+
+namespace Logos
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnidadEducativa.UnidadEdu logos = new UnidadEdu();
+            Console.ReadKey();
+
+            logos.ListarProfesores();
+
+            logos.cursosPreEscolar.ListarCursos();
+
+        }
+    }
+
+}
